614209615c357a74d5ee6dac75ee315459d98246
kyohee <kyohee32@gmail.com>
2022-06-24 00:31:14 +0900

    fffff

---
+++ myproject/fffff.c
@@ -0,0 +1,8 @@
+//
+//  fffff.c
+//  myproject
+//
+//  Created by kyoheeKim on 2022/06/24.
+//
+
+#include "fffff.h"
---
+++ myproject/fffff.h
@@ -0,0 +1,13 @@
+//
+//  fffff.h
+//  myproject
+//
+//  Created by kyoheeKim on 2022/06/24.
+//
+
+#ifndef fffff_h
+#define fffff_h
+
+#include <stdio.h>
+
+#endif /* fffff_h */
