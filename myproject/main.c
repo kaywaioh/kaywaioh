67cdbd9ae6aa04f36a06e113d363814afcd6cef8
kyohee <kyohee32@gmail.com>
2022-06-23 23:28:56 +0900

    Initial Commit

---
+++ myproject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  myproject
+//
+//  Created by kyoheeKim on 2022/06/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
